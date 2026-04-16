87b390b255cc04498c64bc75aaf9a196901378b8
Raul Somesan <raulfabiansome@gmail.com>
2026-04-16 16:31:09 +0300

    PHP Enums

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -10,7 +10,7 @@ class Starship
         private string $name,
         private string $class,
         private string $captain,
-        private string $status
+        private StarshipStatusEnum $status
     ) {
     }
 
@@ -30,7 +30,7 @@ class Starship
     {
         return $this->captain;
     }
-    public function getStatus(): string
+    public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
     }
